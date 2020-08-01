be447d0a0f69a194010a28442c54aea18d8c9bf1
yashydv <68542825+yashydv@users.noreply.github.com>
2020-08-01 23:21:40 +0530

    Add files via upload

---
+++ 30 days coding/d7-array.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+         // Get the input
+         Scanner sc = new Scanner(System.in);
+         int length = sc.nextInt();
+         int[] array = new int[length];
+     
+         for (int i = 0; i < length; i++) {
+             array[i] = sc.nextInt();
+         }
+     
+         // Var holding our new string
+         String result = "";
+     
+         for (int i = array.length - 1; i >= 0; i--) {
+             result = result + array[i] + " ";
+         }
+             System.out.println(result);
+     }
+ }
