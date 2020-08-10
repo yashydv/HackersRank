5ee700be14465470eeaf058bbcb704d1a68f9c41
yashydv <68542825+yashydv@users.noreply.github.com>
2020-08-10 22:39:06 +0530

    Add files via upload

---
+++ 30 days coding/d16-stringTOint.java
@@ -0,0 +1,25 @@
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        
+    String S=sc.nextLine();
+    try
+    {
+        int no;
+        no=Integer.parseInt(S);
+        System.out.println(no);
+    }
+    catch(Exception a)
+    {
+        System.out.println("Bad String");
+    }          
+   }
+  }
